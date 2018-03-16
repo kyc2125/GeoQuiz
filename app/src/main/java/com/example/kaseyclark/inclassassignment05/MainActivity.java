31b7c8319b48e457b80a3f6e84fe707cd09e2e54
Kasey <kyc2125@tc.columbia.edu>
2018-03-15 21:34:39 -0400

    initial commit

---
+++ app/src/main/java/com/example/kaseyclark/inclassassignment05/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.kaseyclark.inclassassignment05;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Button;
+
+
+public class QuizActivity extends AppCompatActivity {
+
+    private int mTextResId;
+    private boolean mAnswer;
+    private Button mTrueButton;
+    private Button mFalseButton;
+    private TextView mQuestionTextView;
+    private Button showAnswerButton;
+
+    public static Intent newIntent(Context packageContext, boolean asnswerIsCorrect);
+    Intent intent =new Intent(packageConext, cheat activity class )
+
+
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/kaseyclark/inclassassignment05/Question.java
@@ -0,0 +1,8 @@
+package com.example.kaseyclark.inclassassignment05;
+
+/**
+ * Created by kaseyclark on 2/19/18.
+ */
+
+public class Question {
+}
