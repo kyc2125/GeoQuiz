a05648a289d797c4c4a2fa7f73d40706d680ea01
Kasey <kyc2125@tc.columbia.edu>
2018-03-18 12:36:20 -0400

    continuing work

--- app/src/main/java/com/example/kaseyclark/inclassassignment05/MainActivity.java
+++ app/src/main/java/com/example/kaseyclark/inclassassignment05/MainActivity.java
@@ -8,26 +8,43 @@ import android.widget.Button;
 import java.util.ArrayList;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+
+{
 
     private int mTextResId;
     private boolean mAnswer;
     private Button mTrueButton;
     private Button mFalseButton;
     private TextView mQuestionTextView;
-    private Button showAnswerButton;
-    private Question [] questionlist=new Question[]{
-            new Question (getString (  R.string.question_africa), false),
-            new Question (getString ( R.string.question_americas), true),
-            new Question(getString ( R.string.question_asia), true),
-            new Question (getString (R.string.question_australia), false),
-            new  Question (getString (R.string.question_midest), false),
-            new Question (getString (R.string.question_oceans), true)
-    };
+    private Button mCheatButton;
+    private int mQustion_Index;
+    private Question [] questionlist=new Question[6];
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       // new Question (getString (  R.string.question_africa), false),
+               questionlist[0] = new Question (getString ( R.string.question_americas), true);
+              //  new Question(getString ( R.string.question_asia), true),
+              //  new Question (getString (R.string.question_australia), false),
+             //   new  Question (getString (R.string.question_midest), false),
+             //   new Question (getString (R.string.question_oceans), true)
+
+         mTrueButton= (Button) findViewById(R.id.true_button);
+         mFalseButton= (Button) findViewById(R.id.false_button);
+         mCheatButton = (Button) findViewById(R.id.cheat_button);
+
+          mQuestionTextView = (TextView)findViewById(R.id.Question);
+          Question mCurrentQuestion= questionlist [mQustion_Index];
+
+          String statement=mCurrentQuestion.getStatement();
+          mQuestionTextView.setText(statement);
+
+
+
 
     }
 
