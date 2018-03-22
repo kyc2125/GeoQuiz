205cfe38a4d1d223adf18292e9cfe863bda1dd05
Kasey <kyc2125@tc.columbia.edu>
2018-03-21 22:03:52 -0400

    Got cheat activity working

--- app/src/main/java/com/example/kaseyclark/inclassassignment05/CheatActivity.java
+++ app/src/main/java/com/example/kaseyclark/inclassassignment05/CheatActivity.java
@@ -1,13 +1,41 @@
 package com.example.kaseyclark.inclassassignment05;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class CheatActivity extends AppCompatActivity {
+
+    private Button cheat_button;
+    private TextView answer_text;
+    private boolean answer_is_true;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
+
+        cheat_button=(Button)findViewById(R.id.show_answer_button);
+        answer_text=(TextView)findViewById(R.id.show_answer_text);
+
+        answer_is_true=getIntent().getBooleanExtra("answer", false);
+
+        cheat_button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                if (answer_is_true == true)
+                    answer_text.setText("True");
+                else
+                    answer_text.setText("False");
+            }
+
+        });
     }
 }
--- app/src/main/java/com/example/kaseyclark/inclassassignment05/MainActivity.java
+++ app/src/main/java/com/example/kaseyclark/inclassassignment05/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.kaseyclark.inclassassignment05;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Button;
+import android.widget.Toast;
+import android.content.Intent;
 
 import java.util.ArrayList;
 
@@ -18,7 +21,8 @@ public class MainActivity extends AppCompatActivity
     private Button mFalseButton;
     private TextView mQuestionTextView;
     private Button mCheatButton;
-    private int mQustion_Index;
+    private int mQuestion_Index;
+    private Button next_button;
     private Question [] questionlist=new Question[6];
 
 
@@ -26,25 +30,85 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       // new Question (getString (  R.string.question_africa), false),
-               questionlist[0] = new Question (getString ( R.string.question_americas), true);
-              //  new Question(getString ( R.string.question_asia), true),
-              //  new Question (getString (R.string.question_australia), false),
-             //   new  Question (getString (R.string.question_midest), false),
-             //   new Question (getString (R.string.question_oceans), true)
+        questionlist[0]= new Question (getString (  R.string.question_africa), false);
+               questionlist[1] = new Question (getString ( R.string.question_americas), true);
+               questionlist[2]= new Question(getString ( R.string.question_asia), true);
+               questionlist[3]= new Question(getString (R.string.question_australia), false);
+               questionlist[4]= new  Question(getString (R.string.question_midest), false);
+                questionlist[5]=new Question(getString (R.string.question_oceans), true);
 
          mTrueButton= (Button) findViewById(R.id.true_button);
          mFalseButton= (Button) findViewById(R.id.false_button);
          mCheatButton = (Button) findViewById(R.id.cheat_button);
+         next_button=(Button) findViewById(R.id.next_button);
 
           mQuestionTextView = (TextView)findViewById(R.id.Question);
-          Question mCurrentQuestion= questionlist [mQustion_Index];
+          Question mCurrentQuestion= questionlist [mQuestion_Index];
 
           String statement=mCurrentQuestion.getStatement();
           mQuestionTextView.setText(statement);
 
+        next_button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+               mQuestion_Index++;
+               if (mQuestion_Index == 5)
+                   mQuestion_Index=0;
+                Question mCurrentQuestion= questionlist [mQuestion_Index];
+
+                String statement=mCurrentQuestion.getStatement();
+                mQuestionTextView.setText(statement);
+            }
 
 
+        });
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                Question mCurrentQuestion= questionlist [mQuestion_Index];
+
+            if (mCurrentQuestion.isStatementTrue())
+                Toast.makeText(MainActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+            else
+                Toast.makeText(MainActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+            }
+
+
+        });
+
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                Question mCurrentQuestion= questionlist [mQuestion_Index];
+
+                if (mCurrentQuestion.isStatementTrue())
+                    Toast.makeText(MainActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(MainActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+            }
+
+
+        });
+
+        mCheatButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                Question mCurrentQuestion= questionlist [mQuestion_Index];
+
+                Intent intent = new Intent (MainActivity.this, CheatActivity.class);
+                intent.putExtra("answer", mCurrentQuestion.isStatementTrue() );
+                startActivity( intent);
+            }
+
+
+        });
 
     }
 
